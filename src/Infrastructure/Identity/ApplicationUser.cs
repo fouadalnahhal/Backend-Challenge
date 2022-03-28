b80c3df58de5d32131526b03f9a035cb5b103e81
FOUAD AHMED <fouad.ahmed118@ci.menofia.edu.eg>
2022-03-28 13:26:02 +0200

    initDB

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -4,4 +4,11 @@ namespace CleanArchitecture.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
+    public Type Type { get; set; }
+    public double Salary { get; set; }
+}
+public enum Type
+{
+    Administartor,
+    Employee
 }
---
+++ src/Infrastructure/Persistence/Migrations/20220328112430_initDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infrastructure.Persistence.Migrations
+{
+    public partial class initDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Salary",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "AspNetUsers");
+        }
+    }
+}
